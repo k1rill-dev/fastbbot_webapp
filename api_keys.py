9c3621c6fa57bc680f39991e0c4ebe7f7afe8cde
k1rill-dev <kirillbelokobylsky@yandex.ru>
2022-11-20 02:49:43 +0300

    final work

--- api_keys.py
+++ api_keys.py
@@ -2,9 +2,12 @@ import datetime
 import time
 
 import fastreport_cloud_sdk
+import requests
 from fastreport_cloud_sdk.rest import ApiException
 from pprint import pprint
 import environ
+
+from base_things import BaseMy
 
 env = environ.Env()
 environ.Env.read_env('.env')
@@ -29,6 +32,7 @@ def get_subscription() -> fastreport_cloud_sdk.models.subscription_vm.Subscripti
         except ApiException as e:
             print("Exception when calling GroupsApi->groups_create_group: %s\n" % e)
 
+
 def get_api_keys():
     with fastreport_cloud_sdk.ApiClient(_config_api(TOKEN)) as api_client:
         api_instance = fastreport_cloud_sdk.ApiKeysApi(api_client)
@@ -40,7 +44,8 @@ def get_api_keys():
         except ApiException as e:
             print("Exception when calling ApiKeysApi->api_keys_get_api_keys: %s\n" % e)
 
-def create_api_key(description:str=None, expired:datetime.date=None):
+
+def create_api_key(description: str = None, expired: datetime.date = None):
     with fastreport_cloud_sdk.ApiClient(_config_api(TOKEN)) as api_client:
         api_instance = fastreport_cloud_sdk.ApiKeysApi(api_client)
         create_api_key_vm = fastreport_cloud_sdk.CreateApiKeyVM(description, expired)
@@ -51,9 +56,26 @@ def create_api_key(description:str=None, expired:datetime.date=None):
         except ApiException as e:
             print("Exception when calling ApiKeysApi->api_keys_create_api_key: %s\n" % e)
 
-print(get_api_keys())
+
+def delete_api_key(key):
+    b = BaseMy('apikey', TOKEN, PROS, 'https://fastreport.cloud')
+
+    headers, sub_id = b._config()
+
+    json = {
+        "apiKey": key
+    }
+
+    response = requests.delete(f'{b._host}/api/manage/v1/ApiKeys', headers=headers, json=json)
+
+
+    print(response.status_code)
+
+# print(get_api_keys())
 date_time_str = '20/11/22'
 
 date = datetime.datetime.strptime(date_time_str, '%d/%m/%y')
 # print(date)
 # create_api_key('test', date)
+delete_api_key('4ndymm59qkqr34fb5eiupc8gxp9g395i7toi6zu58w4p3bdnjxdo')
+
--- users.py
+++ users.py
@@ -1,7 +1,10 @@
 import fastreport_cloud_sdk
+import requests
 from fastreport_cloud_sdk.rest import ApiException
 from pprint import pprint
 import environ
+
+from base_things import BaseMy
 
 env = environ.Env()
 environ.Env.read_env('.env')
@@ -18,30 +21,20 @@ def _config_api(token: str) -> fastreport_cloud_sdk.configuration.Configuration:
     return configuration
 
 
-def get_subscription() -> fastreport_cloud_sdk.models.subscription_vm.SubscriptionVM:
-    with fastreport_cloud_sdk.ApiClient(_config_api(TOKEN)) as api_client:
-        a = fastreport_cloud_sdk.SubscriptionsApi(api_client)
-        try:
-            return a.subscriptions_get_subscription(id=PROS)
-        except ApiException as e:
-            print("Exception when calling GroupsApi->groups_create_group: %s\n" % e)
 
+def upd_current_user(name='null', username='null', email='null', pswd_new='null', pswd_new2='null'):
+    b = BaseMy('apikey', TOKEN, PROS, 'https://fastreport.cloud')
 
-def update_me(name: str = None, username: str = None, email: str = None, password_new: str = None,
-              password_new2: str = None):
-    with fastreport_cloud_sdk.ApiClient(_config_api(TOKEN)) as api_client:
-        # Create an instance of the API class
-        api_instance = fastreport_cloud_sdk.UserProfileApi(api_client)
-        update_user_profile_vm = fastreport_cloud_sdk.UpdateUserProfileVM(name, username,
-                                                                          email, password_new,
-                                                                          password_new2)  # UpdateUserProfileVM |  (optional)
+    headers, sub_id = b._config()
 
-        try:
-            # Update profile of the current user
-            api_instance.user_profile_update_my_profile(update_user_profile_vm=update_user_profile_vm)
-            # print(api_instance.user_profile_get_my_profile())
-        except ApiException as e:
-            print("Exception when calling UserProfileApi->user_profile_update_my_profile: %s\n" % e)
+    json = {
+        "name": name,
+        "username": username,
+
+    }
+    response = requests.put(f'{b._host}/api/manage/v1/UserProfile', headers=headers, json=json)
+    print(response.json())
+
 
 def get_my_profile():
     with fastreport_cloud_sdk.ApiClient(_config_api(TOKEN)) as api_client:
@@ -85,7 +78,5 @@ def update_profile_settings(profile_visibility: str = None, default_subscription
         except ApiException as e:
             print("Exception when calling UserSettingsApi->user_settings_get_current_user_settings: %s\n" % e)
 
-
-# update_me('kirill', '0uts1der')
-get_my_profile()
+# get_my_profile()
 # update_profile_settings(show_hidden_files_and_folders=True)
